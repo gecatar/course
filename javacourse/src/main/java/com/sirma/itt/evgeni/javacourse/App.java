636bbb2838c40f5227e24c6b3fc9c29927a99f29
gecatar <gecatar@gmail.com>
2014-11-17 16:28:35 +0200

    re

---
+++ javacourse/src/main/java/com/sirma/itt/evgeni/javacourse/App.java
@@ -0,0 +1,13 @@
+package com.sirma.itt.evgeni.javacourse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
