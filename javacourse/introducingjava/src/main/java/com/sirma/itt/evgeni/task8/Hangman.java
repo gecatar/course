fe61b2b81d879422e5b784c68ca639594457e6d2
gecatar <gecatar@gmail.com>
2014-11-18 09:12:03 +0200

    re

---
+++ javacourse/introducingjava/src/main/java/com/sirma/itt/evgeni/task8/Hangman.java
@@ -0,0 +1,100 @@
+package com.sirma.itt.evgeni.task8;
+
+import java.util.Random;
+
+/**
+ * Generate random words, count lives, check for open letters.
+ * 
+ * @author Evgeni Stefanov
+ */
+public class Hangman {
+
+	String[] words = { "telephone", "cartrige", "microscope", "torpedo",
+			"aircraft", "inspector", "detective", "mailbox", "elephant" };
+	Random rnd = new Random();
+	String word = new String();
+	String opened = new String();
+	byte wrongGuess = 9;
+
+	/**
+	 * choose random word
+	 */
+	public void getWord() {
+		word = words[Math.abs(rnd.nextInt() % words.length)].toString();
+		wrongGuess = 9;
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i = 0; i < word.length(); i++) {
+			stringBuilder.append("-");
+		}
+		opened = stringBuilder.toString();
+	}
+
+	/**
+	 * Return word that must be opened.
+	 * 
+	 * @return current word.
+	 */
+	public String getAnswer() {
+		return word;
+	}
+
+	/**
+	 * Check word state.
+	 * 
+	 * @return true if word is fully opened.
+	 */
+	public Boolean isOpen() {
+		for (int i = 0; i < opened.length(); i++) {
+			if (opened.charAt(i) == 45) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Check state of player.
+	 * 
+	 * @return number of left lives.
+	 */
+	public byte getWrongGuess() {
+		return wrongGuess;
+	}
+
+	/**
+	 * Print current word. Replace unopened letters whit "-".
+	 * 
+	 * @return string whit word state.
+	 */
+	public String print() {
+		return opened;
+	}
+
+	/**
+	 * Check for letter. Decrement lives.
+	 * 
+	 * @param ch
+	 *            user input
+	 * @return string whit opened letter.
+	 */
+	public String checkLetter(char ch) {
+
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(opened);
+		boolean decrementLife = true;
+		int index = 0;
+		index = word.indexOf(ch, index);
+		while (index != -1 && index < word.length()) {
+			stringBuilder.setCharAt(index, ch);
+			index++;
+			index = word.indexOf(ch, index);
+			decrementLife = false;
+		}
+		if (decrementLife && wrongGuess > 0) {
+			wrongGuess--;
+		}
+		opened = stringBuilder.toString();
+		return opened;
+	}
+
+}
---
+++ javacourse/introducingjava/src/main/java/com/sirma/itt/evgeni/task8/HangmanRunner.java
@@ -0,0 +1,41 @@
+package com.sirma.itt.evgeni.task8;
+
+import com.sirma.itt.evgeni.util.ConsoleReader;
+
+/**
+ * Hangman game You have nine trays.
+ * @author Evgeni Stefanov
+ *
+ */
+public class HangmanRunner {
+
+	/**
+	 * Hangman Runner. Control user input and other stuff.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Hangman hangman = new Hangman();
+		String userChoise = new String();
+		System.out.println("Enter QUIT for EXIT");
+		hangman.getWord();
+		while (userChoise.compareTo("quit") != 0) {
+			System.out.println(hangman.print());
+			System.out.println("You have " + hangman.getWrongGuess() + " trys left!!!");
+			userChoise = ConsoleReader.readString();
+			hangman.checkLetter(userChoise.charAt(0));
+			if (hangman.getWrongGuess() > 0) {
+				if (hangman.isOpen()) {
+					System.out.println("Congrachilation!!!!!!!!!!! Your opene the word!!!");
+					hangman.getWord();
+				}
+			} else {
+				System.out.println("You are death!!!!");
+				System.out.println("The word is: "+hangman.getAnswer());
+				hangman.getWord();
+			}
+		}
+
+	}
+
+}
