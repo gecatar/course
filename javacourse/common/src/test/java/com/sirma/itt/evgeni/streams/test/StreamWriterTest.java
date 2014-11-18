bd290883b581e949f6a6c8329c8e817e67363c33
gecatar <gecatar@gmail.com>
2014-11-18 17:05:07 +0200

    er

---
+++ javacourse/common/src/test/java/com/sirma/itt/evgeni/streams/test/StreamWriterTest.java
@@ -0,0 +1,20 @@
+package com.sirma.itt.evgeni.streams.test;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+
+import org.junit.Test;
+
+import com.sirma.itt.evgeni.streams.StreamWriter;
+
+public class StreamWriterTest {
+
+	@Test
+	public void test() {
+		StreamWriter streamWriter = new StreamWriter();
+		ByteArrayOutputStream ost = new ByteArrayOutputStream();
+		fail("Not yet implemented");
+	}
+
+}
