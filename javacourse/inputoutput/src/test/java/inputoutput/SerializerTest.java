397530890a6aa019ced3fa0bd356d0776d5c4f10
gecatar <gecatar@gmail.com>
2014-11-19 16:31:37 +0200

    Refactoring...

---
+++ javacourse/inputoutput/src/test/java/inputoutput/SerializerTest.java
@@ -0,0 +1,14 @@
+package inputoutput;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SerializerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
