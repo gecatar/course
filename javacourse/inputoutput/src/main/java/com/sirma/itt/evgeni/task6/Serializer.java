ffe96b0ba32e322b080c5b00c6ac110f62fb767a
gecatar <gecatar@gmail.com>
2014-11-19 16:12:37 +0200

    refactoring......

--- javacourse/inputoutput/src/main/java/com/sirma/itt/evgeni/task6/Serializer.java
+++ javacourse/inputoutput/src/main/java/com/sirma/itt/evgeni/task6/Serializer.java
@@ -1,66 +1,65 @@
 package com.sirma.itt.evgeni.task6;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.nio.file.Path;
 
+import com.sirma.itt.evgeni.task6.DataClass;
+
 /**
- * Serialize object and save it to file.
+ * Read and save objects.
+ * 
  * @author Evgeni Stefanov
- *
+ * 
  */
 public class Serializer {
+
 	/**
+	 * Read objects from passed file.
 	 * 
-	 * @param path to file where object will be saved.
-	 * @param o object that will be serialized.
-	 * @throws IOException
+	 * @param path
+	 * @return
 	 */
-	public static void saveObject(Path path, DataClass o) throws IOException {
-		FileOutputStream fos = null;
-		ObjectOutputStream oos = null;
+	public Object getObject(Path path) {
 		try {
-			fos = new FileOutputStream(path.toString());
-			oos = new ObjectOutputStream(fos);
-			oos.writeObject(o);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} finally {
-			if (fos != null) {
-				fos.close();
+			ObjectInputStream objectStream = null;
+			try {
+				objectStream = new ObjectInputStream(new BufferedInputStream(
+						new FileInputStream(path.toString())));
+				return objectStream.readObject();
+			} finally {
+				objectStream.close();
 			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		return null;
 	}
 
 	/**
-	 * Read object from file.
-	 * @param path file that will be read.
-	 * @return DataClass object.
-	 * @throws IOException
+	 * Save object to file.
+	 * @param dataClass
+	 * @param path
 	 */
-	public static DataClass getObject(Path path) throws IOException {
-
-		FileInputStream fis = null;
-		ObjectInputStream ois = null;
-		
+	public void saveObject(DataClass dataClass, Path path) {
 		try {
-			fis = new FileInputStream(path.toString());
-			ois = new ObjectInputStream(fis);
-			return (DataClass) ois.readObject();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found!!!");
-		} catch (ClassNotFoundException e) {
-			System.out.println("Error while reading object!!!");
-			e.printStackTrace();
-		} finally{
-			if(ois!=null){
-				fis.close();
+			ObjectOutputStream outputStream = null;
+			try {
+				outputStream = new ObjectOutputStream(new BufferedOutputStream(
+						new FileOutputStream(path.toString())));
+				outputStream.writeObject(dataClass);
+			} finally {
+				outputStream.close();
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		return null;
 	}
 }
