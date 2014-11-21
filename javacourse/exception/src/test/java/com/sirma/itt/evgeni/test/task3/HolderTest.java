a7e9c91665715dfcb70ce1f4666c0cbd393b06d8
GecaTM <GecaTM@GecaTM-PC.zyxel.com>
2014-11-21 18:28:11 +0200

    Test refactoring....

--- javacourse/exception/src/test/java/com/sirma/itt/evgeni/test/task3/HolderTest.java
+++ javacourse/exception/src/test/java/com/sirma/itt/evgeni/test/task3/HolderTest.java
@@ -12,17 +12,21 @@ import com.sirma.itt.evgeni.task3.Holder;
  *
  */
 public class HolderTest {
-
+	
 	@Test
-	public void test() {
+	public void addElementTest(){
 		Holder holder = new Holder(2);
-		assertFalse(holder.remove());
-		assertTrue(holder.addElement("test"));
-		assertTrue(holder.addElement("test"));
-		assertFalse(holder.addElement("test"));
-		assertTrue(holder.remove());
-		assertTrue(holder.remove());
-		assertFalse(holder.remove());
+		assertTrue(holder.addElement("  "));
+		assertTrue(holder.addElement("  "));
+		assertFalse(holder.addElement(" "));
 	}
-
+	
+	@Test
+	public void removeElementTest(){
+		Holder holder = new Holder(2);
+		holder.addElement(" ");
+		assertTrue(holder.remove());
+		assertFalse(holder.remove());
+		
+	}
 }
