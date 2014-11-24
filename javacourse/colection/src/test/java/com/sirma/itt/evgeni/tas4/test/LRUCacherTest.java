7dd44cd8c8420f2558ff632191e403dde9b850bd
gecatar <gecatar@gmail.com>
2014-11-24 14:01:41 +0200

    re

--- javacourse/colection/src/test/java/com/sirma/itt/evgeni/tas4/test/LRUCacherTest.java
+++ javacourse/colection/src/test/java/com/sirma/itt/evgeni/tas4/test/LRUCacherTest.java
@@ -2,26 +2,58 @@ package com.sirma.itt.evgeni.tas4.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.sirma.itt.evgeni.task4.Cacher;
 
 public class LRUCacherTest {
 
+	Cacher cacher;
+	Object[] object;
+	
+	@Before
+	public void setUp() throws Exception {
+		
+		object = new Object[10];
+		
+	}
+	
+	/**
+	 * Add element in not full list.
+	 */
 	@Test
-	public void test() {
-		Cacher cacher = new Cacher(2);
-		Object[] obj = new Object[10];
-		for (int i = 0; i < 10; i++) {
-			obj[i] = new Integer(i);
-		}
-		cacher.addElement(0, obj[0]);
-		cacher.addElement(1, obj[1]);
-		assertTrue(cacher.toString().equals("1 0 "));
-		cacher.addElement(2, obj[2]);
-		assertTrue(cacher.toString().equals("2 1 "));
-		cacher.addElement(2, obj[2]);
-		assertTrue(cacher.toString().equals("2 1 "));
+	public void putInNotFullListTest(){
+		cacher = new Cacher(2);
+		cacher.addElement(0, object[0]);
+		String expectedCache = "0 ";
+		String actualCache = cacher.toString();
+		assertEquals(expectedCache, actualCache);
+		cacher.addElement(1, object[1]);
+		expectedCache = "1 0 ";
+		actualCache = cacher.toString();
+		assertEquals(expectedCache, actualCache);
+	}
+
+	/**
+	 * Check behavior when adding elements in full list.
+	 */
+	@Test
+	public void putInFullListTest() {
+		cacher = new Cacher(2);
+		cacher.addElement(0, object[0]);
+		String expectedCache = "0 ";
+		String actualCache = cacher.toString();
+		assertEquals(expectedCache, actualCache);
+		cacher.addElement(1, object[1]);
+		expectedCache = "1 0 ";
+		actualCache = cacher.toString();
+		assertEquals(expectedCache, actualCache);
+		cacher.addElement(2, object[2]);
+		expectedCache = "2 1 ";
+		actualCache = cacher.toString();
+		assertEquals(expectedCache, actualCache);
+		
 	}
 
 }
