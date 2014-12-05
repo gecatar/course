bd3a2512c7364ca90dc20529c00bac28b6486935
gecatar <gecatar@gmail.com>
2014-12-05 14:31:04 +0200

    Refactoring.....

---
+++ javacourse/chatapplication/src/main/java/com/sirma/itt/evgeni/client/ClientApp.java
@@ -0,0 +1,46 @@
+package com.sirma.itt.evgeni.client;
+
+import com.sirma.itt.evgeni.comunication.ComunicatorListener;
+import com.sirma.itt.evgeni.comunication.UILIstener;
+
+public class ClientApp implements UILIstener, ComunicatorListener {
+
+	public void ClientApp() {
+
+	}
+
+	public void setConectionStatus(boolean conected) {
+		// TODO Auto-generated method stub client
+
+	}
+
+	public void showMesage(String name, String text) {
+		// TODO Auto-generated method stub client
+
+	}
+
+	public void addUser(String name) {
+		// TODO Auto-generated method stub client
+
+	}
+
+	public void removeUser(String name) {
+		// TODO Auto-generated method stub client
+
+	}
+
+	public void startConection(String nickname, String ip, String port) {
+		// TODO Auto-generated method stub interface
+
+	}
+
+	public void stopConection() {
+		// TODO Auto-generated method stub interface
+
+	}
+
+	public void sendMessage(String name, String message) {
+		// TODO Auto-generated method stub interface
+
+	}
+}
---
+++ javacourse/chatapplication/src/main/java/com/sirma/itt/evgeni/server/ServerApp.java
@@ -0,0 +1,46 @@
+package com.sirma.itt.evgeni.server;
+
+import com.sirma.itt.evgeni.comunication.ComunicatorListener;
+import com.sirma.itt.evgeni.comunication.UILIstener;
+
+public class ServerApp implements UILIstener, ComunicatorListener {
+
+	public ServerApp() {
+
+	}
+
+	public void setConectionStatus(boolean conected) {
+		// TODO Auto-generated method stub server
+
+	}
+
+	public void showMesage(String name, String text) {
+		// TODO Auto-generated method stub server
+
+	}
+
+	public void addUser(String name) {
+		// TODO Auto-generated method stub server
+
+	}
+
+	public void removeUser(String name) {
+		// TODO Auto-generated method stub server
+
+	}
+
+	public void startConection(String nickname, String ip, String port) {
+		// TODO Auto-generated method stub interface
+
+	}
+
+	public void stopConection() {
+		// TODO Auto-generated method stub interface
+
+	}
+
+	public void sendMessage(String name, String message) {
+		// TODO Auto-generated method stub interface
+
+	}
+}
