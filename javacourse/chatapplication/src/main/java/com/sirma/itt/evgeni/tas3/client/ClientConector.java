3a8e2c4bd976c835ab0f4ca0f278036e5a4e05d1
gecatar <gecatar@gmail.com>
2014-12-08 16:49:22 +0200

    addet new project...

---
+++ javacourse/chatapplication/src/main/java/com/sirma/itt/evgeni/tas3/client/ClientConector.java
@@ -0,0 +1,25 @@
+package com.sirma.itt.evgeni.tas3.client;
+
+import com.sirma.itt.evgeni.comunication.Comunicator;
+import com.sirma.itt.evgeni.comunication.Conector;
+
+public class ClientConector extends Conector {
+
+	public ClientConector(Comunicator comunicator, String ipAdress, int port) {
+		super(comunicator, ipAdress, port);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setAutoclosable(AutoCloseable autoClosable) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
