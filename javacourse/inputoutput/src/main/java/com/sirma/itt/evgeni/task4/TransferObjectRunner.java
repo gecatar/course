e0ad6ef144d79edfc69e31e734d2b3d764ad9b27
gecatar <gecatar@gmail.com>
2014-12-17 10:43:29 +0200

    re

--- javacourse/inputoutput/src/main/java/com/sirma/itt/evgeni/task4/TransferObjectRunner.java
+++ javacourse/inputoutput/src/main/java/com/sirma/itt/evgeni/task4/TransferObjectRunner.java
@@ -1,13 +1,28 @@
 package com.sirma.itt.evgeni.task4;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import com.sirma.itt.evgeni.util.TransferObject;
+
 /**
  * Tests are placed in common project.
+ * 
  * @author Evgeni Stefanov
- *
+ * 
  */
 public class TransferObjectRunner {
 
 	public static void main(String[] args) {
-		
+		ByteArrayOutputStream byteOutputStream = new ByteArrayOutputStream();
+		ByteArrayInputStream byteInputStream = new ByteArrayInputStream(
+				new byte[] { 1, 2, 3, 4, 5, 6, 7 });
+		TransferObject transverObject = new TransferObject(byteInputStream,
+				byteOutputStream);
+
+		transverObject.transfer(7);
+		transverObject.close();
+
 	}
 
 }
