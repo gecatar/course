7f71e66d253dd1cb749be83350d7305d988b670a
gecatar <gecatar@gmail.com>
2014-12-19 12:26:02 +0200

    refactoring

---
+++ javacourse/designpattern/src/test/java/com/sirma/itt/evgeni/task1/test/AbstractFactoryTest.java
@@ -0,0 +1,5 @@
+package com.sirma.itt.evgeni.task1.test;
+
+public class AbstractFactoryTest {
+
+}
---
+++ javacourse/designpattern/src/test/java/com/sirma/itt/evgeni/task6/test/StockContainerTest.java
@@ -0,0 +1,19 @@
+package com.sirma.itt.evgeni.task6.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StockContainerTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
