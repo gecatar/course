51cc6b4c48ebe3b5421a6e724ef85a1adf510ff8
GecaTM <GecaTM@GecaTM-PC>
2014-12-20 22:30:47 +0200

    re...

--- javacourse/thread/src/main/java/com/sirma/itt/evgeni/task5/Holder.java
+++ javacourse/thread/src/main/java/com/sirma/itt/evgeni/task5/Holder.java
@@ -1,6 +1,8 @@
 package com.sirma.itt.evgeni.task5;
 
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Save strings in massive. Execute two operations. Add element and remove
@@ -11,12 +13,15 @@ import java.util.ArrayList;
  */
 public class Holder {
 
+	private static final Logger LOGGER = Logger.getLogger(Holder.class
+			.getName());
+
 	public Holder(int count) {
 		this.count = count;
 	}
 
-	ArrayList<String> strings = new ArrayList<String>();
-	int count;
+	private ArrayList<String> strings = new ArrayList<String>();
+	private int count;
 
 	/**
 	 * Add element in massive. Generate random string whit given length. If
@@ -26,13 +31,14 @@ public class Holder {
 	public synchronized void addElement(String string) {
 		if (strings.size() < count) {
 			strings.add(string);
-			System.out.println("New element addet!!!");
+			LOGGER.log(Level.INFO, "New element added.");
 			notifyAll();
 		} else {
-			System.out.println("List is full!!!");
+			LOGGER.log(Level.INFO, "List is full.");
 			try {
 				wait();
 			} catch (InterruptedException e) {
+				LOGGER.log(Level.SEVERE, "Thread interupted", e);
 			}
 			addElement(string);
 		}
@@ -43,17 +49,30 @@ public class Holder {
 	 * boundary exception. Print how much free space left.
 	 */
 	public synchronized void remove() {
-		if(strings.size()>0){
-			strings.remove(strings.size()-1);
-			System.out.println("Last elements from list removed...");
+		if (strings.size() > 0) {
+			strings.remove(strings.size() - 1);
+			LOGGER.log(Level.INFO, "Last element from list removed.");
 			notifyAll();
-		}else{
+		} else {
 			try {
 				wait();
 			} catch (InterruptedException e) {
+				LOGGER.log(Level.SEVERE, "Thread interupted", e);
 			}
 			remove();
 		}
-		
+
+	}
+
+	/**
+	 * Print all elements on holder.
+	 */
+	@Override
+	public String toString() {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (String string : strings) {
+			stringBuilder.append("element:").append(string).append("\n");
+		}
+		return stringBuilder.toString().trim();
 	}
 }
---
+++ javacourse/thread/src/test/java/com/sirma/itt/evgeni/task5/test/HolderTest.java
@@ -0,0 +1,36 @@
+package com.sirma.itt.evgeni.task5.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.sirma.itt.evgeni.task5.Holder;
+
+public class HolderTest {
+
+	Holder holder;
+
+	/**
+	 * Test adding elements functioning.
+	 */
+	@Test
+	public void addElementTest() {
+		holder = new Holder(2);
+		holder.addElement("test1");
+		holder.addElement("test2");
+		assertEquals("element:test1\nelement:test2", holder.toString());
+	}
+
+	/**
+	 * Test correct removing on objects.
+	 */
+	@Test
+	public void removeElementTest() {
+		holder = new Holder(2);
+		holder.addElement("test1");
+		holder.addElement("test2");
+		holder.remove();
+		assertEquals("element:test1", holder.toString());
+	}
+
+}
