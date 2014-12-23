60bb415b15e2573e59cee966e04bcca690832f7e
GecaTM <GecaTM@GecaTM-PC.zyxel.com>
2014-12-23 17:01:52 +0200

    ijii

--- javacourse/networkgui/src/main/java/com/sirma/itt/evgeni/task1/CalculatorApp.java
+++ javacourse/networkgui/src/main/java/com/sirma/itt/evgeni/task1/CalculatorApp.java
@@ -1,12 +1,16 @@
 package com.sirma.itt.evgeni.task1;
 
+import java.math.BigDecimal;
+
 public class CalculatorApp {
 
-	Calculator calculator = new Calculator();
-	CalculatorView calculatorView = new CalculatorView();
+	private Calculator calculator = new Calculator();
+	private CalculatorView calculatorView = new CalculatorView();
+	private BigDecimal currentResult = new BigDecimal("0");
 
-	public void calculateOperation(Operation operation) {
-		calculator.calculate(operation);
+	public void calculate(String number, char operation) {
+		calculator.calculate(new Operation(currentResult,
+				new BigDecimal(number), operation));
 	}
 
 	/**
