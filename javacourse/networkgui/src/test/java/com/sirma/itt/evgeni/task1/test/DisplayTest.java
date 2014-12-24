e8a103cc1c557d7069ba4211682d52c359034463
GecaTM <GecaTM@GecaTM-PC.zyxel.com>
2014-12-24 19:07:38 +0200

    u8y7

---
+++ javacourse/networkgui/src/test/java/com/sirma/itt/evgeni/task1/test/DisplayTest.java
@@ -0,0 +1,19 @@
+package com.sirma.itt.evgeni.task1.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DisplayTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assertEquals("", "");
+	}
+
+}
