b553b58398e3cdfa3bd84baa56944fd32e59bdd3
GecaTM <GecaTM@GecaTM-PC>
2015-01-01 12:57:11 +0200

    iopiop

--- javacourse/inputoutput/src/test/java/com/sirma/itt/evgeni/task3/test/ReverseFileTest.java
+++ javacourse/inputoutput/src/test/java/com/sirma/itt/evgeni/task3/test/ReverseFileTest.java
@@ -3,9 +3,9 @@ package com.sirma.itt.evgeni.task3.test;
 import static org.junit.Assert.assertEquals;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 
 import org.junit.Test;
 
@@ -17,33 +17,12 @@ public class ReverseFileTest {
 
 	/**
 	 * Check correct storing on data to file.
-	 * 
-	 * @throws IOException
-	 * @throws URISyntaxException
 	 */
 	@Test
 	public void saveTextTest() throws IOException, URISyntaxException {
 		String text = "123456";
 		File file = new File(getClass().getResource("/test.txt").toURI());
-		fileReverser.saveText(file, text);
-		assertEquals("123456", fileReverser.getText(file));
-	}
-
-	/**
-	 * Check correct reversing on file.
-	 * 
-	 * @throws FileNotFoundException
-	 * @throws IOException
-	 * @throws URISyntaxException
-	 */
-	@Test
-	public void reverseTextTest() throws IOException, URISyntaxException {
-		File file = new File(getClass().getResource("/test.txt").toURI());
-		String text = "123456";
-		fileReverser.saveText(file, text);
-		String expectedText = "654321";
 		fileReverser.reverseFile(file.getPath());
-		String actualText = fileReverser.getText(file);
-		assertEquals(expectedText, actualText);
+		assertEquals(text.getBytes(), Files.readAllBytes(file.toPath()));
 	}
 }
