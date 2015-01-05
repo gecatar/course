4b27e5fda6385fa97d49101a43778a3eea7f2b02
gecatar <gecatar@gmail.com>
2015-01-05 09:58:30 +0200

    vb bv

--- javacourse/exception/src/main/java/com/sirma/itt/evgeni/task2/ReadIntervalRunner.java
+++ javacourse/exception/src/main/java/com/sirma/itt/evgeni/task2/ReadIntervalRunner.java
@@ -1,8 +1,6 @@
 package com.sirma.itt.evgeni.task2;
 
-import com.sirma.itt.evgeni.interfaces.UserInputReader;
 import com.sirma.itt.evgeni.util.ConsoleReader;
-import com.sirma.itt.evgeni.util.InputReader;
 import com.sirma.itt.evgeni.util.ValueNotInRangeException;
 
 /**
@@ -20,21 +18,18 @@ public class ReadIntervalRunner {
 	 */
 	public static void main(String[] args) {
 
-	
-			System.out
-					.println("Verify user input. Must be in range (min - max).");
-			System.out.println("Enter minimum value");
-			int min = ConsoleReader.readInt();
-			System.out.println("Enter maximum value");
-			int max = ConsoleReader.readInt();
-			System.out.println("Enter value:");
-			int value = ConsoleReader.readInt();
-			try {
-				RangeValidator.chekIsInRange(min, max, value);
-			} catch (ValueNotInRangeException e) {
-				e.printStackTrace();
-			}
-	
-		
+		System.out.println("Verify user input. Must be in range (min - max).");
+		System.out.println("Enter minimum value");
+		int min = ConsoleReader.readInt();
+		System.out.println("Enter maximum value");
+		int max = ConsoleReader.readInt();
+		System.out.println("Enter value:");
+		int value = ConsoleReader.readInt();
+		try {
+			RangeValidator.chekIsInRange(min, max, value);
+		} catch (ValueNotInRangeException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
