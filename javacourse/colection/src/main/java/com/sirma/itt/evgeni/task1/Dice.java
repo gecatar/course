12c81992fa80d7dfa68ef58f8b4d6185a3814746
GecaTM <GecaTM@GecaTM-PC.zyxel.com>
2015-01-05 19:39:04 +0200

    uiyuio

--- javacourse/colection/src/main/java/com/sirma/itt/evgeni/task1/Dice.java
+++ javacourse/colection/src/main/java/com/sirma/itt/evgeni/task1/Dice.java
@@ -2,20 +2,20 @@ package com.sirma.itt.evgeni.task1;
 
 /**
  * Generate random value. User define dice sides.
+ * 
  * @author Evgeni Stefanov
  *
  */
 public class Dice {
 
 	private int sides;
-	
-	public Dice(int sides){
+
+	public Dice(int sides) {
 		this.sides = sides;
 	}
 
 	/**
 	 * Generate random value.
-	 * @return random dice value.
 	 */
 	public int rool() {
 		return (int) (Math.random() * sides) + 1;
