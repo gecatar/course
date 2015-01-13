ca5641faf27014a653b85dcf60591271e2a68946
GecaTM <GecaTM@GecaTM-PC.zyxel.com>
2015-01-13 12:11:27 +0200

    5t54t45

--- javacourse/networkgui/src/main/java/com/sirma/itt/evgeni/task2/DownloadProgressListener.java
+++ javacourse/networkgui/src/main/java/com/sirma/itt/evgeni/task2/DownloadProgressListener.java
@@ -1,12 +1,30 @@
 package com.sirma.itt.evgeni.task2;
 
+/**
+ * Allow listening for download events triggered from download agent.
+ * 
+ * @author GecaTM
+ *
+ */
 public interface DownloadProgressListener {
 
+	/**
+	 * Set progress.
+	 */
 	public void setProgress(int progress);
 
+	/**
+	 * Set file size.
+	 */
 	public void setFileSize(long fileSize);
 
+	/**
+	 * Set current download size.
+	 */
 	public void setDownloadedSize(long downloadedSize);
 
+	/**
+	 * Set download status.
+	 */
 	public void setDownloadStatus(boolean compleeted);
 }
