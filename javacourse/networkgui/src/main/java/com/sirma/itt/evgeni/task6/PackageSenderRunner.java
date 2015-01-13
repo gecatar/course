a4bdc376b328abfa9e3b3da35e3dbd0273301155
GecaTM <GecaTM@GecaTM-PC.zyxel.com>
2015-01-13 14:37:41 +0200

    rtgrtg

--- javacourse/networkgui/src/main/java/com/sirma/itt/evgeni/task6/PackageSenderRunner.java
+++ javacourse/networkgui/src/main/java/com/sirma/itt/evgeni/task6/PackageSenderRunner.java
@@ -11,18 +11,17 @@ public class PackageSenderRunner {
 				"228.5.6.7", 7005);
 		DatagramTransmiter extendedDataGramTransmiter = new DatagramTransmiter(
 				"228.5.6.8", 7005);
-		PackageSender packageSender = new PackageSender(
-				basicDataGramTransmiter, extendedDataGramTransmiter);
+
 		int choise = 0;
 		while (choise != 9) {
 			choise = ConsoleReader.readInt();
 			if (choise == 1) {
 				System.out.println("Sending 8 byte package.");
-				packageSender.send(new byte[8]);
+				// packageSender.send(new byte[8]);
 			}
 			if (choise == 2) {
 				System.out.println("Sending 32 byte package.");
-				packageSender.send(new byte[32]);
+				// packageSender.send(new byte[32]);
 			}
 		}
 	}
