564b5e0cda6757116a47c14dead5a7de677cd952
GecaTM <GecaTM@GecaTM-PC.zyxel.com>
2015-01-15 12:26:02 +0200

    iooioij

--- javacourse/comunication/src/main/java/com/sirma/itt/evgeni/client/UserList.java
+++ javacourse/comunication/src/main/java/com/sirma/itt/evgeni/client/UserList.java
@@ -5,6 +5,12 @@ import java.awt.event.MouseListener;
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
 
+/**
+ * List whit connected users.
+ * 
+ * @author GecaTM
+ *
+ */
 public class UserList extends JList<String> {
 
 	private DefaultListModel<String> listModel;
@@ -15,18 +21,29 @@ public class UserList extends JList<String> {
 		addMouseListener(mouseListener);
 	}
 
+	/**
+	 * Add use.
+	 */
 	public void addUser(String name) {
 		if (!listModel.contains(name)) {
 			listModel.addElement(name);
 		}
 	}
 
+	/**
+	 * Remove user.
+	 */
 	public void removeUser(String name) {
 		if (listModel.contains(name)) {
 			listModel.removeElement(name);
 		}
 	}
 
+	/**
+	 * Get selected name.
+	 * 
+	 * @return
+	 */
 	public String getSelectedName() {
 		return listModel.getElementAt(getSelectedIndex());
 	}
