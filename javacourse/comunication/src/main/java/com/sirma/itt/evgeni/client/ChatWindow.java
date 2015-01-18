5cd7cf7f06178a5c6e5d04fc37163bf4971e9607
GecaTM <GecaTM@GecaTM-PC.zyxel.com>
2015-01-18 13:50:25 +0200

    dqwdqw

---
+++ javacourse/comunication/src/main/java/com/sirma/itt/evgeni/client/ChatWindow.java
@@ -0,0 +1,97 @@
+package com.sirma.itt.evgeni.client;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JTabbedPane;
+
+import com.sirma.itt.evgeni.comunication.ComunicatorApp;
+import com.sirma.itt.evgeni.comunication.MesageCommand;
+import com.sirma.itt.evgeni.comunication.Window;
+
+public class ChatWindow extends Window {
+
+	private final JTabbedPane tabbedPane = new JTabbedPane();
+	private final MessageNotifyer notifyer;
+	private ConectionPanel conectionPanel;
+	private ConversationPanel conversationPanel;
+	private UserList userList;
+
+	public ChatWindow(ComunicatorApp listener) {
+		super(listener);
+		conectionPanel = new ConectionPanel(this, null);
+		notifyer = new MessageNotifyer(this);
+		add(tabbedPane);
+	}
+
+	@Override
+	public void setConectionStatus(MesageCommand status) {
+
+	}
+
+	@Override
+	public void showMesage(String name, String text) {
+
+	}
+
+	@Override
+	public void addUser(String name) {
+
+	}
+
+	@Override
+	public void removeUser(String name) {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public String getIPField() {
+		return null;
+	}
+
+	@Override
+	public int getPort() {
+		return 0;
+	}
+
+	@Override
+	public String getMessageText(String name) {
+		return null;
+	}
+
+}
